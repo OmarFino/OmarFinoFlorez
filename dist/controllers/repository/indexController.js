cb5914590ed456edb5d8c661ef470282ad2d00fc
OmarFino <42946884+OmarFino@users.noreply.github.com>
2022-07-12 18:44:27 -0500

    ultimo

--- dist/controllers/repository/indexController.js
+++ dist/controllers/repository/indexController.js
@@ -24,18 +24,45 @@ exports.getRepository = getRepository;
 const getRepositorysTribe = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const id = parseInt(req.params.id);
-        const response = yield dataBase_1.client.query(`select repository.id_repository, 
+        const data = yield dataBase_1.client.query(`select repository.id_repository, 
         repository.name, tribe.name as tribe, organization.name as organization, metrics.coverage,metrics.code_smell, 
         metrics.bugs, metrics.vulnerabilities, metrics.hotspot, repository.statu as verificationState from organization
         inner join tribe on organization.pk_id_organization = tribe.fk_id_organization
         inner join repository on tribe.pk_id_tribe = repository.id_tribe
         inner join metrics on repository.id_repository = metrics.fk_id_repository
         where tribe.pk_id_tribe = ${id}`);
+        const response = yield dataBase_1.client.query(`select repository.id_repository, 
+        repository.name, tribe.name as tribe, organization.name as organization, metrics.coverage,metrics.code_smell, 
+        metrics.bugs, metrics.vulnerabilities, metrics.hotspot, repository.statu as verificationState from organization
+        inner join tribe on organization.pk_id_organization = tribe.fk_id_organization
+        inner join repository on tribe.pk_id_tribe = repository.id_tribe
+        inner join metrics on repository.id_repository = metrics.fk_id_repository
+        where tribe.pk_id_tribe = ${id} and repository.state = 'E' and  metrics.coverage >= 75 `);
         response.rows.forEach(element => {
-            console.log(element);
+            if (element.verificationstate == 604) {
+                element.verificationstate = "Verificado";
+            }
+            else {
+                if (element.verificationstate == 605) {
+                    element.verificationstate = "En espera";
+                }
+                else {
+                    if (element.verificationstate == 606) {
+                        element.verificationstate = "Aprobado";
+                    }
+                    else {
+                        element.verificationstate = "estado no existe";
+                    }
+                }
+            }
         });
-        if (response.rows != "") {
-            return res.status(200).json({ repositories: response.rows });
+        if (data.rows != "") {
+            if (response.rows != "") {
+                return res.status(200).json({ repositories: response.rows });
+            }
+            else {
+                return res.status(200).json({ message: 'La Tribu no tiene repositorios que cumplan con la cobertura necesaria' });
+            }
         }
         else {
             return res.status(200).json({ message: 'La Tribu no se encuentra registrada' });
